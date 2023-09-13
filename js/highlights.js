664f004a6414597fd56cdefacc786fe7030a7c51
rafsywhj <70297474+rafsywhj@users.noreply.github.com>
2023-09-14 00:20:21 +0400

    Update highlights.js

--- js/highlights.js
+++ js/highlights.js
@@ -8,22 +8,22 @@ AFRAME.registerComponent("cursor-listener", {
   },  
   handleMouseEnterEvents: function () {
     // Mouse Enter Events
-    this.el.addEventListener("mouseenter", () => {
-      const id = this.el.getAttribute("id");
-      const postersId = [
-        "superman",
-        "spiderman",
-        "captain-aero",
-        "outer-space",
-      ];
-      if (postersId.includes(id)) {
-        const postersContainer = document.querySelector("#posters-container");
-        postersContainer.setAttribute("cursor-listener", {
-          selectedItemId: id,
-        });
-        this.el.setAttribute("material", { color: "#1565c0" });
-      }
-    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
   },
   handleMouseLeaveEvents: function () {
     // Mouse Leave Events
